ba21c6120427f624d5f01eb4f4630f99b9d4549c
Elena <smirnova.eu@gmail.com>
2022-05-22 14:24:40 +0300

    new modules, tests

---
+++ 2021-05/anki-training-system/letter-models/src/main/java/com/example/lettermodels/DictionaryForRepeat.java
@@ -0,0 +1,25 @@
+package com.example.lettermodels;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "repeat_letter")
+public class DictionaryForRepeat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "words_count")
+    private long wordsCount;
+}
--- 2021-05/anki-training-system/letter-models/src/test/java/com/example/lettermodels/LetterModelsApplicationTests.java
+++ 2021-05/anki-training-system/letter-models/src/test/java/com/example/lettermodels/LetterModelsApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.trainingsystem;
+package com.example.lettermodels;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TrainingSystemApplicationTests {
+class LetterModelsApplicationTests {
 
     @Test
     void contextLoads() {
